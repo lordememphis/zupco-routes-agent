88ea076992b547c4f39470d3de5fd8954ace0e39
Memphis <muteket@gmail.com>
2021-08-19 14:42:38 +0200

    fix: Fix typo

--- src/app/routes/account/reports/transaction-history/transaction-history.component.ts
+++ src/app/routes/account/reports/transaction-history/transaction-history.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: ` <main>
     <div class="py-6 sm:px-6 lg:px-8">
       <h1 class="text-3xl font-bold leading-tight text-gray-900">
-        Transaction Histroy Report
+        Transaction History Report
       </h1>
       <router-outlet></router-outlet>
     </div>
